c4c27ba02da7ba79ebc782dc3aa728332ac62f45
Scott Edwards <scott@scottmedwards.com>
2016-04-19 13:43:06 -0400

    initial commit

---
+++ myblog/admin.py
@@ -0,0 +1,13 @@
+from copy import deepcopy
+from django.contrib import admin
+from mezzanine.blog.admin import BlogPostAdmin
+from mezzanine.blog.models import BlogPost
+
+blog_fieldsets = deepcopy(BlogPostAdmin.fieldsets)
+blog_fieldsets[0][1]["fields"].insert(-2, "service_icon")
+
+class MyBlogPostAdmin(BlogPostAdmin):
+    fieldsets = blog_fieldsets
+
+admin.site.unregister(BlogPost)
+admin.site.register(BlogPost, MyBlogPostAdmin)
---
+++ myblog/defaults.py
@@ -0,0 +1,10 @@
+from mezzanine.conf import register_setting
+
+register_setting(
+    name="BLOG_POST_PER_PAGE",
+    label="Blog posts per page",
+    description="The number of blog posts to show per page.",
+    editable=True,
+    default=12,
+)
+
